aaf60627b87bc46ce54c7a342e3c3c2c75d71cf7
AnomanX Dell <patoeng@gmail.com>
2022-03-12 18:39:37 +0700

    pre qual ok

--- PPAGUI/Main.Designer.cs
+++ PPAGUI/Main.Designer.cs
@@ -365,10 +365,7 @@ namespace PPAGUI
             // 
             this.ResourceDataGroup.Panel.Controls.Add(this.btnResetState);
             this.ResourceDataGroup.Panel.Controls.Add(this.label2);
-            this.ResourceDataGroup.Panel.Controls.Add(this.Dt_MoveOut);
-            this.ResourceDataGroup.Panel.Controls.Add(this.label6);
             this.ResourceDataGroup.Panel.Controls.Add(this.label30);
-            this.ResourceDataGroup.Panel.Controls.Add(this.Dt_MoveIn);
             this.ResourceDataGroup.Panel.Controls.Add(this.Tb_PO);
             this.ResourceDataGroup.Panel.Controls.Add(this.Tb_ContainerPosition);
             this.ResourceDataGroup.Panel.Controls.Add(this.Tb_PumpSerialNumber);
@@ -821,72 +818,6 @@ namespace PPAGUI
             this.TimerRealtime.Enabled = true;
             this.TimerRealtime.Interval = 10000;
             this.TimerRealtime.Tick += new System.EventHandler(this.TimerRealtime_Tick);
-            // 
-            // Dt_MoveIn
-            // 
-            this.Dt_MoveIn.CalendarTodayDate = new System.DateTime(2021, 12, 29, 0, 0, 0, 0);
-            this.Dt_MoveIn.CustomFormat = "yyyy-MM-dd hh:mm:ss tt";
-            this.Dt_MoveIn.Enabled = false;
-            this.Dt_MoveIn.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.Dt_MoveIn.Location = new System.Drawing.Point(11, 159);
-            this.Dt_MoveIn.Name = "Dt_MoveIn";
-            this.Dt_MoveIn.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.ProfessionalSystem;
-            this.Dt_MoveIn.ShowUpDown = true;
-            this.Dt_MoveIn.Size = new System.Drawing.Size(173, 33);
-            this.Dt_MoveIn.StateCommon.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.Dt_MoveIn.StateCommon.Border.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.Dt_MoveIn.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
-            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
-            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
-            this.Dt_MoveIn.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            this.Dt_MoveIn.StateCommon.Border.ImageStyle = ComponentFactory.Krypton.Toolkit.PaletteImageStyle.Inherit;
-            this.Dt_MoveIn.StateCommon.Border.Rounding = 20;
-            this.Dt_MoveIn.StateCommon.Border.Width = 1;
-            this.Dt_MoveIn.TabIndex = 58;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.Gray;
-            this.label6.Location = new System.Drawing.Point(8, 143);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(139, 16);
-            this.label6.TabIndex = 84;
-            this.label6.Text = "Move In: (TimeStamp)";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.Gray;
-            this.label8.Location = new System.Drawing.Point(190, 143);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(149, 16);
-            this.label8.TabIndex = 86;
-            this.label8.Text = "Move Out: (TimeStamp)";
-            // 
-            // Dt_MoveOut
-            // 
-            this.Dt_MoveOut.CalendarTodayDate = new System.DateTime(2021, 12, 29, 0, 0, 0, 0);
-            this.Dt_MoveOut.CustomFormat = "yyyy-MM-dd hh:mm:ss tt";
-            this.Dt_MoveOut.Enabled = false;
-            this.Dt_MoveOut.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.Dt_MoveOut.Location = new System.Drawing.Point(193, 159);
-            this.Dt_MoveOut.Name = "Dt_MoveOut";
-            this.Dt_MoveOut.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.ProfessionalSystem;
-            this.Dt_MoveOut.ShowUpDown = true;
-            this.Dt_MoveOut.Size = new System.Drawing.Size(186, 33);
-            this.Dt_MoveOut.StateCommon.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.Dt_MoveOut.StateCommon.Border.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.Dt_MoveOut.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
-            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
-            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
-            this.Dt_MoveOut.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            this.Dt_MoveOut.StateCommon.Border.ImageStyle = ComponentFactory.Krypton.Toolkit.PaletteImageStyle.Inherit;
-            this.Dt_MoveOut.StateCommon.Border.Rounding = 20;
-            this.Dt_MoveOut.StateCommon.Border.Width = 1;
-            this.Dt_MoveOut.TabIndex = 85;
             // 
             // Main
             // 
